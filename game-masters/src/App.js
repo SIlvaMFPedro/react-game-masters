cf57564fa749620dd1d91365bd8ed7ae3889e96d
silvamfpedro <silva.mfpedro@gmail.com>
2022-09-28 17:10:47 +0100

    Added changes to App

--- game-masters/src/App.js
+++ game-masters/src/App.js
@@ -1,24 +1,456 @@
 import logo from './logo.svg';
-import './App.module.css';
+import styles from './App.module.css';
+import {BrowserRouter, Routes, Route, useNavigate, useLocation, useLoaderData} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Browse from './Containers/Browse/Browse';
+import GamePage from './Containers/GamePage/GamePage';
+import NotFound from './Containers/NotFound/NotFound';
+import Home from "./Containers/Home/Home";
+import filterNames from "./utils/filterNames";
+import games from "./utils/games";
+import templateGame from "./utils/templateGame";
+import {clear} from "@testing-library/user-event/dist/clear";
 
 function App() {
+  const [currentFilter, setCurrentFilter] = useState("none");
+  const [allGames, setAllGames] = useState(games);
+  const [cart, setCart] = useState([]);
+  const [cartAmount, setCartAmount] = useState(0);
+  const [shownGames, setShownGames] = useState(allGames);
+  const [reviewDisplay, setReviewDisplay] = useState(false);
+  const [cartDisplayed, setCartDisplayed] = useState(false);
+  const [search, setSearch] = useState("");
+  const [overlap, setOverlap] = useState(false);
+  const [searching, setSearching] = useState(false);
+  const [browsing, setBrowsing] = useState(true);
+  const [selectedGame, setSelectedGame] = useState(false);
+  const [extended, setExtended] = useState(false);
+  const [textExtended, setTextExtended] = useState(false);
+  const [hoverState, setHoverState] = useState([
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    },
+    {
+      hovered: false,
+      selected: false
+    }
+  ]);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  if (location.pathname !== "/game-masters/" && location.pathname !== "/game-masters/browse" && selectedGame === false){
+    let surname = location.pathname.substring(29);
+    let currentGame = games.find(game => game.surname === surname);
+    if (currentGame !== undefined){
+      setSelectedGame(currentGame);
+    }
+    else{
+      setSelectedGame(templateGame);
+    }
+  }
+
+  async function handleBrowse(){
+    setExtended(false);
+    setTextExtended(false);
+    setCartDisplayed(false);
+    setHoverState([...hoverState, hoverState[21].hovered = false]);
+    navigate(('/game-masters/browse'));
+  }
+
+  const handleHome = () => {
+    setExtended(false);
+    setTextExtended(false);
+    setCartDisplayed(false);
+    setHoverState([...hoverState, hoverState[21].hovered = false]);
+    navigate(('/game-masters/'));
+  }
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    setSearching(false);
+  }
+
+  const handleSearchSubmit = (e) => {
+    setCurrentFilter("none");
+    e.preventDefault();
+    setSearching(true);
+    if (location.pathname !== "/game-masters/browse"){
+      navigate('/game-masters/browse');
+    }
+  }
+
+  const handleSelect = (e) => {
+    setCurrentFilter(filterNames[e.target.id - 8]);
+  }
+
+  const handleSelectGame = (e) => {
+    if (e.target.tagName === "BUTTON"){
+      return;
+    }
+    else if(e.target.classList[0] !== "AddToCart_addToCart_zbJPe"){
+      setSelectedGame(games[e.target.parentNode.id]);
+      navigate(`/game-masters/games/${games[e.target.parentNode.id].surname}`);
+    }
+  }
+
+  const handleLike = (e) => {
+    let handledLike = allGames.map((game, i) => {
+      if (e.target.id === i){
+        game.isLiked = !game.isLiked;
+        return game;
+      }
+      else{
+        return game;
+      }
+    });
+    setAllGames(handledLike);
+  }
+
+  const clearFilter = () => {
+    setCurrentFilter("none");
+    setSearch("");
+    setReviewDisplay(false);
+  }
+
+  const openGamePage = (e) => {
+    setCartDisplayed(false);
+    let selectedGameSurname = e.target.id;
+    navigate(`/game-masters/games/${selectedGameSurname}`);
+  }
+
+  const handleHover = (e) => {
+    if (hoverState[e.target.id].selected){
+      return;
+    }
+    let newHoverState = hoverState.map((element, i) => {
+      if (e.target.id === i){
+        element.hovered = !element.hovered;
+        return element;
+      }
+      else{
+        return element;
+      }
+    });
+    setHoverState(newHoverState);
+  }
+
+  const handleHoverGame = (e) => {
+    let handleHoveredGame = allGames.map((game, i) => {
+      if (e.target.id === i){
+        game.isHovered = !game.isHovered;
+        return game;
+      }
+      else{
+        return game;
+      }
+    });
+    setAllGames(handleHoveredGame);
+  }
+
+  const handleAddToCart = (e) => {
+    let handledAddedGame = allGames.map((game, i) => {
+      if (location.pathname === "/game-masters/browse"){
+        if (e.target.id === i){
+          game.inCart = true;
+          let newCart = cart;
+          newCart.push(game);
+          setCart(newCart);
+          setCartAmount(cartAmount + 1);
+          return game;
+        }
+        else{
+          return game;
+        }
+      }
+      else{
+        if (selectedGame.id === i){
+          game.inCart = true;
+          let newCart = cart;
+          newCart.push(game);
+          setCart(newCart);
+          setCartAmount(cartAmount + 1);
+          return game;
+        }
+        else{
+          return game;
+        }
+      }
+    });
+    setAllGames(handledAddedGame);
+  }
+
+  const clearCart = () => {
+    setCart([]);
+    setCartAmount(0);
+    const defaultGames = allGames.map((game, i) => {
+      game.inCart = false;
+      game.isHovered = false;
+      return game;
+    });
+    setAllGames(defaultGames);
+    let newHoverState = hoverState[21];
+    newHoverState.hovered = false;
+    setHoverState([...hoverState, hoverState[21] = newHoverState]);
+  }
+
+  const handleRemoveFromCart = (e) => {
+    let removedIndex = cart.findIndex(game => game.id === e.target.id);
+    let newAllGames = allGames.map((game, i) => {
+      if (game.id === e.target.id){
+        game.inCart = false;
+        game.isHovered = false;
+        return game;
+      }
+      else{
+        return game;
+      }
+    });
+    setAllGames(newAllGames);
+    let firstHalf = cart.slice(0, removedIndex);
+    let secondHalf = cart.slice(removedIndex + 1);
+    let addedUp = firstHalf.concat(secondHalf);
+    setCart(addedUp);
+    setCartAmount(cartAmount - 1);
+    setHoverState([...hoverState, hoverState[21].hovered = false]);
+  }
+
+  useEffect(() => {
+    setOverlap(false);
+    if (location.pathname === "/game-masters/"){
+      setBrowsing(false);
+    }
+    else{
+      setBrowsing(true);
+    }
+    if (location.pathname !== "/game-masters/browse"){
+      document.body.style.overflow = "hidden";
+    }
+    else if (location.pathname === "/game-masters/browse"){
+      document.body.style.overflow = "scroll";
+    }
+  }, [location.pathname])
+
+  const handleOpenCart = () => {
+    setCartDisplayed(true);
+  }
+
+  const handleCloseCart = () => {
+    setCartDisplayed(false);
+  }
+
+  useEffect(() => {
+    console.log(selectedGame);
+  }, [selectedGame]);
+
+  useEffect(() => {
+    if (cartDisplayed) {
+      document.body.style.overflow = "hidden !important";
+    }
+    else{
+      document.body.style.overflow = "scroll !important";
+    }
+  }, [cartDisplayed]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence exitBeforeEnter>
+      <Routes key={location.pathname} location={location}>
+        <Route path="/game-masters/"
+               element={<Home
+                   handleHover={handleHover}
+                   hoverState={hoverState}
+                   shownGames={shownGames}
+                   cart={cart}
+                   cartAmount={cartAmount}
+                   cartDisplayed={cartDisplayed}
+                   handleOpenCart={handleOpenCart}
+                   handleCloseCart={handleCloseCart}
+                   clearCart={clearCart}
+                   handleAddToCart={handleAddToCart}
+                   handleLike={handleLike}
+                   handleHoverGame={handleHoverGame}
+                   handleSelectGame={handleSelectGame}
+                   handleRemoveFromCart={handleRemoveFromCart}
+                   setHoverState={setHoverState}
+                   overlap={overlap}
+                   setOverlap={setOverlap}
+                   openGamePage={openGamePage}/>}/>
+        <Route path="/game-masters/browse"
+               element={<Browse
+                 cart={cart}
+                 cartAmount={cartAmount}
+                 handleHover={handleHover}
+                 handleSelect={handleSelect}
+                 hoverState={hoverState}
+                 currentFilter={currentFilter}
+                 shownGames={shownGames}
+                 setShownGames={setShownGames}
+                 clearFilter={clearFilter}
+                 reviewDisplay={reviewDisplay}
+                 setReviewDisplay={setReviewDisplay}
+                 allGames={allGames}
+                 setAllGames={setAllGames}
+                 handleLike={handleLike}
+                 handleHoverGame={handleHoverGame}
+                 handleAddToCart={handleAddToCart}
+                 handleSelectGame={handleSelectGame}
+                 handleSearch={handleSearch}
+                 handleSearchSubmit={handleSearchSubmit}
+                 search={search}
+                 searching={searching}
+                 browsing={browsing}
+                 handleBrowse={handleBrowse}
+                 handleHome={handleHome}
+                 cartDisplayed={cartDisplayed}
+                 handleOpenCart={handleOpenCart}
+                 handleCloseCart={handleCloseCart}
+                 clearCart={clearCart}
+                 handleRemoveFromCart={handleRemoveFromCart}
+                 setHoverState={setHoverState}
+                 openGamePage={openGamePage}/>}/>
+        <Route path="/game-masters/games/:gameId"
+               element={<GamePage
+                   cart={cart}
+                   cartAmount={cartAmount}
+                   handleHover={handleHover}
+                   hoverState={hoverState}
+                   handleLike={handleLike}
+                   handleAddToCart={handleAddToCart}
+                   handleSelectGame={handleSelectGame}
+                   selectedGame={selectedGame}
+                   setSelectedGame={setSelectedGame}
+                   handleSearch={handleSearch}
+                   handleSearchSubmit={handleSearchSubmit}
+                   search={search}
+                   searching={searching}
+                   browsing={browsing}
+                   handleBrowse={handleBrowse}
+                   handleHome={handleHome}
+                   setHoverState={setHoverState}
+                   allGames={allGames}
+                   extended={extended}
+                   setExtended={setExtended}
+                   textExtended={textExtended}
+                   setTextExtended={setTextExtended}
+                   cartDisplayed={cartDisplayed}
+                   handleOpenCart={handleOpenCart}
+                   handleCloseCart={handleCloseCart}
+                   clearCart={clearCart}
+                   handleRemoveFromCart={handleRemoveFromCart}
+                   openGamePage={openGamePage}/>}/>
+        <Route path="*" element={<NotFound
+            cartDisplayed={cartDisplayed}
+            handleCloseCart={handleCloseCart}
+            handleOpenCart={handleOpenCart}
+            cartAmount={cartAmount}
+            clearCart={clearCart}
+            hoverState={hoverState}
+            handleHome={handleHome}
+            handleHover={handleHover}
+            cart={cart}
+            browsing={browsing}
+            search={search}
+            searching={searching}
+            handleSearch={handleSearch}
+            handleSearchSubmit={handleSearchSubmit}
+            handleBrowse={handleBrowse}
+            handleRemoveFromCart={handleRemoveFromCart}
+            openGamePage={openGamePage}/>}/>
+      </Routes>
+    </AnimatePresence>
   );
 }
 
